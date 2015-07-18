c525b0e584ae0bf3bcb79e5b88a80703db455cbc
zhangkecyrus <kzhan013@uottawa.ca>
2015-07-17 21:07:27 -0400

    studynote update

--- public/javascripts/controllers/Studynote-controller.js
+++ public/javascripts/controllers/Studynote-controller.js
@@ -5,9 +5,9 @@ angular.module('Studynote-controller')
 				// code here. 
 				var stage = new createjs.Stage("demoCanvas");
 				var circle = new createjs.Shape();
-				circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-				circle.x = 100;
-				circle.y = 100;
+				circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 30);
+				circle.x = 50;
+				circle.y = 50;
 				stage.addChild(circle);
 				createjs.Tween.get(circle, { loop: true })
 				  .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
@@ -17,6 +17,29 @@ angular.module('Studynote-controller')
 				  .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
 				createjs.Ticker.setFPS(60);
 				createjs.Ticker.addEventListener("tick", stage);
+
+
+				//haha
+			 //    var mystage = new createjs.Stage("haha");
+			 //    var mycircle = new createjs.Shape();
+			 //    mycircle.graphics.beginFill("#FF0").drawPolyStar(100, 100, 50, 5, 0.6, -90);			    //Set position of Shape instance.
+			 //    mycircle.x = mycircle.y = 50;
+			 //    mystage.addChild(mycircle);
+			 //    mystage.update();
+
+				
+				// // mycircle.addEventListener("mousedown", handleTick);
+				// createjs.Ticker.addEventListener("tick", handleTick);
+			 //    function handleTick() {
+			 //     	//Circle will move 10 units to the right.
+			 //        mycircle.x += 10;
+			 //        //Will cause the mycircle to wrap back
+			 //        if (mycircle.x > stage.canvas.width) { mycircle.x = 0; }
+			 //        mystage.update();
+			 //    }				
+				// function handlePress(event) {	
+				//     alert("mousemove");
+				// }
 			}
 		}
 	])
