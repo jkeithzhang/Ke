fbf45ea7af5ad197bcc84404ec78af3061b67cf0
zhangkecyrus <kzhan013@uottawa.ca>
2016-06-20 10:39:58 -0400

    scrollmagic

--- public/javascripts/scrollmagic/myscrollmagic.js
+++ public/javascripts/scrollmagic/myscrollmagic.js
@@ -1,8 +1,25 @@
-// init controller
+// // init controller
 var controller = new ScrollMagic.Controller();
-var scene = new ScrollMagic.Scene({
-					triggerElement: "#trigger1"
-				})
-				.setTween("#animate1", 0.5, {x:100}) // trigger a TweenMax.to tween
-				// .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
-				.addTo(controller);
+// var scene = new ScrollMagic.Scene({
+// 					triggerElement: "#trigger1"
+// 				})
+// 				.setTween("#s2", 0.5, {x:100, opacity:0}) // trigger a TweenMax.to tween
+// 				// .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+// 				.addTo(controller);
+// 				
+windowHeight = $(window).innerHeight();        
+
+new ScrollMagic.Scene({triggerElement: '#s2', 
+                 duration: windowHeight/3}
+               )
+                .setTween( TweenMax.to("span", 1, {ease: Linear.easeInOut, autoAlpha: 1, x:100}) )
+                .addTo(controller);
+
+// var scene2 = new ScrollMagic.Scene({
+//   triggerHook: "onLeave",
+//   triggerElement: "#s2",
+//   // duration: windowHeight*4.5
+//   duration: 10
+// })
+// .setPin("#s2")
+// .addTo(controller);
